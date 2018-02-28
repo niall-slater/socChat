a0204225bd65eccdba13f51314f491afd8f2fd4a
Niall Slater <niallslater@gmail.com>
2018-02-28 16:04:21 +0000

    Reverted

--- index.js
+++ index.js
@@ -2,35 +2,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var people = {};
-
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(client) {
-    console.log(client.id + ' connected.');
+io.on('connection', function(socket) {
+    console.log('A user connected.');
     
-    client.on('set nickname', function(nick) {
-        console.log(client.id + "changed name to " + nick);
-        people[client.id] = nick;
-        client.emit('update', "You have joined the server.");
-        io.sockets.emit('update', nick + " has joined the server.");
-        io.sockets.emit('update-people', people);
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
     });
     
-    client.on('chat message', function(msg) {
-        io.emit('chat message', people[client.id], msg);
-    });
-    
-    client.on('disconnect', function() {
-        io.sockets.emit("update", people[client.id] + " has left the server.");
-        delete people[client.id];
-        io.sockets.emit("update-people", people);
-        console.log(client.id + ' disconnected.');
+    socket.on('disconnect', function() {
+       console.log('User disconnected.');
     });
 });
 
-http.listen(3001, function() {
-    console.log('listening on *:3001');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
