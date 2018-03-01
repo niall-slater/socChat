45bfa030f20f9481e603ece91ea2bb1931bcdd76
Niall Slater <niallslater@gmail.com>
2018-03-01 14:47:46 +0000

    Still working on user logging.

--- index.js
+++ index.js
@@ -13,12 +13,24 @@ io.on('connection', function(socket) {
     people[socket.id] = socket.id;
     console.log(people[socket.id] + ' connected.');
     
+    for (var i = 0; i < people.length; i++) {
+        io.emit('user join', people[i]);
+    }
+    
     socket.on('name change', function(nick) {
         people[socket.id] = nick;
         io.emit('chat message', socket.id + " changed name to " + nick);
         console.log(socket.id + " changed name to " + nick);
         io.emit('user join', nick);
     });
+    
+    socket.on('name load', function(nick) {
+        people[socket.id] = nick;
+        io.emit('chat message', nick + " joined the chat.");
+        console.log(nick + " joined the chat.");
+        io.emit('user join', nick);
+    });
+    
     
     socket.on('chat message', function(msg) {
         io.emit('chat message', people[socket.id] + ": " + msg);
