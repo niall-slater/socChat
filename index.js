7b98c72b70aaec56bf1b7e2bdab149a85e6e3ce2
Niall Slater <niallslater@gmail.com>
2018-03-06 14:19:33 +0000

    Userlist now has loaded usernames and chat history appears to work.

--- index.js
+++ index.js
@@ -16,23 +16,30 @@ io.on('connection', function(socket) {
     
 	io.emit('refresh list', people);
     io.emit('populate messages', messageHistory);
-	
-    for (var i = 0; i < people.length; i++) {
-        io.emit('user join', people[i]);
-    }
     
-    socket.on('name change', function(nick) {
-        var oldName = people[socket.id];
-        people[socket.id] = nick;
-        io.emit('chat message', oldName + " changed name to " + nick);
-        console.log(oldName + " changed name to " + nick);
+    socket.on('name init', function(username) {
+        var idName = people[socket.id];
+        console.log("Socket " + idName + " assigned username " + username);
+        people[socket.id] = username;
+        io.emit('refresh list', people);
     });
     
-    socket.on('name load', function(nick) {
-        people[socket.id] = nick;
-        io.emit('chat message', nick + " joined the chat.");
-        console.log(nick + " joined the chat.");
-        io.emit('user join', nick);
+    socket.on('name change', function(username) {
+        
+        var oldName = people[socket.id];
+        people[socket.id] = username;
+        io.emit('chat message', oldName + " changed name to " + username);
+        console.log(oldName + " changed name to " + username);
+        io.emit('refresh list', people);
+    });
+    
+    socket.on('name load', function(username) {
+        
+        console.log(people[socket.id] + " loaded username " + username);
+        
+        people[socket.id] = username;
+        
+        io.emit('chat message', username + " joined the chat.");
         io.emit('refresh list', people);
     });
     
